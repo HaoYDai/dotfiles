4fa7e9f5438c43422f958daeaf495849dc0c4338
Darren <Darren_Serious.hotmail.com>
2023-06-13 08:58:48 +0800

    feat(fish): add aliases and PATH

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,6 +9,20 @@ set -g theme_display_user yes
 set -g theme_hide_hostname no
 set -g theme_hostname always
 
+# aliases
+alias ls "ls -p -G"
+alias la "ls -A"
+alias ll "ls -l"
+alias lla "ll -A"
+alias g git
+command -qv nvim && alias vim nvim
+
+set -gx EDITOR nvim
+
+set -gx PATH bin $PATH
+set -gx PATH ~/bin $PATH
+set -gx PATH ~/.local/bin $PATH
+
 # Homebrew
 fish_add_path --path /opt/homebrew/bin
 export HOMEBREW_NO_INSTALL_CLEANUP=TRUE
