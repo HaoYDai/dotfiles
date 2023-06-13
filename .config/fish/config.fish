4d1a4b21566767f984032f5bfe701db0a7063055
Darren <Darren_Serious.hotmail.com>
2023-06-13 08:53:25 +0800

    feat(fish): set theme

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,15 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
+set fish_greeting ""
+
+set -gx TERM xterm-256color
+
+# theme
+set -g theme_color_scheme terminal-dark
+set -g fish_prompt_pwd_dir_length 1
+set -g theme_display_user yes
+set -g theme_hide_hostname no
+set -g theme_hostname always
+
+# Homebrew
 fish_add_path --path /opt/homebrew/bin
 export HOMEBREW_NO_INSTALL_CLEANUP=TRUE
 
