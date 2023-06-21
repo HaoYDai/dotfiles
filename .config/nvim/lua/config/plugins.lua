59af95ef15fb0423e4582d2a1f7a61a93ce31191
Darren <Darren_Serious.hotmail.com>
2023-06-21 11:05:48 +0800

    feat(nvim): add lspsaga

--- .config/nvim/lua/config/plugins.lua
+++ .config/nvim/lua/config/plugins.lua
@@ -34,6 +34,18 @@ return {
       "onsails/lspkind.nvim"
     }
   },
+  {
+    "glepnir/lspsaga.nvim",
+    event = "LspAttach",
+    config = function()
+      require("lspsaga").setup({})
+    end,
+    dependencies = {
+      { "nvim-tree/nvim-web-devicons" },
+      --Please make sure you install markdown and markdown_inline parser
+      { "nvim-treesitter/nvim-treesitter" }
+    }
+  },
 
   {
     "nvim-lualine/lualine.nvim",
