feaedd075dd9e3f3ea25d82f24501570f798b173
Darren <Darren_Serious.hotmail.com>
2023-06-21 10:37:33 +0800

    feat(nvim): add cmp and delete lazy-lock.json

--- .config/nvim/lua/config/plugins.lua
+++ .config/nvim/lua/config/plugins.lua
@@ -21,4 +21,15 @@ return {
     "neovim/nvim-lspconfig",
     dependencies = { "williamboman/mason-lspconfig.nvim" }
   },
+  {
+    event = "VeryLazy",
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "neovim/nvim-lspconfig",
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline"
+    }
+  },
 }
