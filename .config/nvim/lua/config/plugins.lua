95a4f470e033a9649c81acf6d2c929b8b47070a0
Darren <Darren_Serious.hotmail.com>
2023-06-21 11:08:09 +0800

    fix(nvim): cmp <CR> error

--- .config/nvim/lua/config/plugins.lua
+++ .config/nvim/lua/config/plugins.lua
@@ -31,7 +31,8 @@ return {
       "hrsh7th/cmp-buffer",
       "hrsh7th/cmp-path",
       "hrsh7th/cmp-cmdline",
-      "onsails/lspkind.nvim"
+      "onsails/lspkind.nvim",
+      "L3MON4D3/LuaSnip"
     }
   },
   {
