df0b731804480438fbf9f4e06cf1f4e521b12414
Darren <Darren_Serious.hotmail.com>
2023-06-28 11:04:21 +0800

    refactor(nvim): refactor lsp-colors, lspkind and lspsaga

--- .config/nvim/lua/plugins/tmp/plugins.lua
+++ .config/nvim/lua/plugins/tmp/plugins.lua
@@ -1,30 +1,4 @@
-  -- Telescope
-  {
-    --    cmd = "Telescope",
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.1',
-    dependencies = { 'nvim-lua/plenary.nvim' }
-  },
-  {
-    "nvim-telescope/telescope-file-browser.nvim",
-    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
-  },
-
-  -- LSP
-
-  {
-    -- event = "VeryLazy",
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      "neovim/nvim-lspconfig",
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "hrsh7th/cmp-cmdline",
-      "onsails/lspkind.nvim",
-      "L3MON4D3/LuaSnip"
-    }
-  },
+ 
   {
     "glepnir/lspsaga.nvim",
     event = "LspAttach",
