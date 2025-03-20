8bbfb5ac3d5188d2366552c3146c6b45a9a83fdf
Darren <Darren_Serious.hotmail.com>
2025-03-20 21:41:51 +0800

    feat(nvim): remove lazy.extras, add lsp

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
-vim.keymap.set("n", "-", "<cmd>Oil --float<CR>", {desc="Open Parent Directory in Oil"})
+vim.keymap.set("n", "-", "<cmd>Oil --float<CR>", { desc = "Open Parent Directory in Oil" })
+vim.keymap.set("n", "gl", function()
+    vim.diagnostic.open_float()
+end, { desc = "Open Diagnostics in Float" })
